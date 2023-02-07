b6dd6ed97ff2ecb2d74b99f17276faef828d5a4c
unknown <dushyantsirohi100@gmail.com>
2023-02-07 08:10:22 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conditionaldirective';
+
+  videos=[
+    {title:'My Video 1', share:415,likes:8257,dislikes:812,thumbnail:'assets/img1.jpg'},
+    {title:'My Video 2', share:215,likes:1257,dislikes:412,thumbnail:'assets/img2.jpg'},
+    {title:'My Video 3', share:515,likes:7257,dislikes:312,thumbnail:'assets/img3.jpg'}
+  ]
+
+  mostLikedVideo = this.getmostlikedVideo();
+
+  getmostlikedVideo(){
+    let videoCopy=[...this.videos];
+    return videoCopy.sort((curr,next)=>next.likes-curr.likes)[0];
+  }
 }
